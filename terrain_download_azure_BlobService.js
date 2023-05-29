44652ff5b66b42ca5da86e3af650bd957916124f
project-88288 <chaiya.88288@gmail.com>
2023-05-29 14:43:08 +0700

    update

--- terrain_download_azure_BlobService.js
+++ terrain_download_azure_BlobService.js
@@ -1,13 +1,8 @@
 #!/usr/bin/env node
 
-require('dotenv').config();
-const { BlobServiceClient } = require("@azure/storage-blob");
-const fs = require("fs").promises;
 const { downloadBlobToLocal } = require("./lib/azure_blobService")
 
 const containerName = 'terrain'
-const storageAccountConnectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
-const blobServiceClient = BlobServiceClient.fromConnectionString(storageAccountConnectionString);
 
 async function main() {
 
