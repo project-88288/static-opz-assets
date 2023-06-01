ba518dccd07ec957cbea2796f614771807a9dd73
project-88288 <chaiya.88288@gmail.com>
2023-06-01 18:03:08 +0700

    update

--- lib/jsonFiles.ts
+++ lib/jsonFiles.ts
@@ -4,36 +4,68 @@ require('dotenv').config();
 import * as fs from 'fs-extra';
 const path = require('path');
 
-export interface objectList {"localterra":{},"mainnet":{},"testnet":{},"classic":{}}
-export const objectTemplate:objectList = {"localterra":{},"mainnet":{},"testnet":{},"classic":{}}
+export const networks = ["mainnet", "testnet", , "classic", "localterra"]
 
-export interface arrayList {"localterra":string[],"mainnet":string[],"testnet":string[],"classic":string[]}
-export const arrayTemplate:arrayList = {"localterra":[],"mainnet":[],"testnet":[],"classic":[]}
+export const JsonArray = () => {
+    let obj = {}
+    networks.forEach(element => {
+        if (!!element) obj[element] = []
+    })
+    return obj
+}
 
-export function getJsonPath(JsonName:string,configPath: string) {
+export const JsonObject = () => {
+    let obj = {}
+    networks.forEach(element => {
+        if (!!element) obj[element] = {}
+    })
+    return obj
+}
+
+export interface objectList { "localterra": {}, "mainnet": {}, "testnet": {}, "classic": {} }
+export const objectTemplate: objectList = { "localterra": {}, "mainnet": {}, "testnet": {}, "classic": {} }
+
+export interface arrayList { "localterra": string[], "mainnet": string[], "testnet": string[], "classic": string[] }
+export const arrayTemplate: arrayList = { "localterra": [], "mainnet": [], "testnet": [], "classic": [] }
+
+export function getJsonPath(JsonName: string, configPath: string) {
     const folder = process.env.TEMP_FOLDER
     const basePath = `${JsonName}.json`
     const parentPath = path.dirname(configPath);
-    const jsonPath = path.join(parentPath,folder,basePath)
+    const jsonPath = path.join(parentPath, folder, basePath)
     return jsonPath
 }
 
-export function getJPath(JsonName:string,configPath: string) {
+export function getJPath(JsonName: string, configPath: string) {
     const folder = process.env.TEMP_FOLDER
     const basePath = `${JsonName}.js`
     const parentPath = path.dirname(configPath);
-    const jsonPath = path.join(parentPath,folder,basePath)
+    const jsonPath = path.join(parentPath, folder, basePath)
     return jsonPath
 }
 
-export async function loadJson(template:any,JsonPath:string):Promise<any> {
+export async function loadJson(template: any, JsonPath: string): Promise<any> {
     if ((await fs.pathExists(JsonPath))) {
-       return await fs.readJSON(JsonPath)
+        return await fs.readJSON(JsonPath)
     }
     else
-    return template
+        return template
 }
 
-export async function storeJson(JsonObject:any,JsonPath:string) {
-    await fs.writeJSON(JsonPath,JsonObject)
+export async function storeJson(JsonObject: any, JsonPath: string) {
+    await fs.writeJSON(JsonPath, JsonObject)
 }
+
+
+/*
+const x = createInterfaceObject(JsonObject)
+
+type ObjectType<T> = {
+    [K in keyof T]: T[K];
+};
+
+function createInterfaceObject<T>(obj: T): ObjectType<T> {
+    return obj;
+}
+
+*/
--- tasks/generate_icons.ts
+++ tasks/generate_icons.ts
@@ -2,6 +2,7 @@ import { task } from "@terra-money/terrain";
 import { arrayTemplate, getJsonPath, loadJson, objectTemplate, storeJson } from "../lib/jsonFiles";
 import fs from 'fs';
 import path from 'path';
+import { StoreCodeProposal } from "@terra-money/terra.js";
 
 task(async () => {
   const networkname = process.env.network
@@ -32,51 +33,75 @@ async function generate_png(type) {
     //
     const srcPath = configPath.replace('config.terrain.json', `logos/LOGO.${type}`)
     console.log(`From this source ${srcPath}`)
-    //
-    const ringcw20Namejsonpath = getJsonPath('ringcw20Names', configPath)
-    const ringcw20names = await loadJson(arrayTemplate, ringcw20Namejsonpath)
-    console.log(`load ring-names from ${ringcw20Namejsonpath}`)
-    //
-    let ringnames: string[] = []
-    Object.entries(ringcw20names[networkname]).forEach(data => {
-      const element = String(data[1]);
-      ringnames.push(element)
-    })
-  
-    for (let index = 0; index < ringnames.length; index++) {
-      const element = ringnames[index];
-      const desPath = srcPath.replace(`logos/LOGO.${type}`, `icons/${element}.${type}`)
+    const directoryPath = path.dirname(srcPath)
+    await createDirectoryIfNotExists(directoryPath)
+    // 
+    const exists = isFilesExists(srcPath)
+    console.log(`File ${srcPath} exists: ${exists}`);
+    if (exists) {
+      //
+      const ringcw20Namejsonpath = getJsonPath('ringcw20Names', configPath)
+      const ringcw20names = await loadJson(arrayTemplate, ringcw20Namejsonpath)
+      console.log(`load ring-names from ${ringcw20Namejsonpath}`)
+      //
+      let ringnames: string[] = []
+      Object.entries(ringcw20names[networkname]).forEach(data => {
+        const element = String(data[1]);
+        ringnames.push(element)
+      })
 
-      await copyFile(srcPath,desPath)
-      .then(() => {
-        console.log(`Copy ${srcPath} to ${desPath}`)
-      }).catch(e => console.log(`${e}`))
+      for (let index = 0; index < ringnames.length; index++) {
+        const element = ringnames[index];
+        const desPath = srcPath.replace(`logos/LOGO.${type}`, `icons/${element}.${type}`)
+
+        await copyFile(srcPath, desPath)
+          .then(() => {
+            console.log(`Copy ${srcPath} to ${desPath}`)
+          }).catch(e => console.log(`${e}`))
+      }
+
+      const starcw20Namejsonpath = getJsonPath('starcw20Names', configPath)
+      const starcw20names = await loadJson(arrayTemplate, starcw20Namejsonpath)
+      console.log(`load star-names from ${ringcw20Namejsonpath}`)
+      //
+      let starnames: string[] = []
+      Object.entries(starcw20names[networkname]).forEach(data => {
+        const element = String(data[1]);
+        starnames.push(element)
+
+      })
+
+      for (let index = 0; index < starnames.length; index++) {
+        const element = starnames[index];
+        const desPath = srcPath.replace(`logos/LOGO.${type}`, `icons/${element}.${type}`)
+        await copyFile(srcPath, desPath)
+          .then(() => {
+            console.log(`Copy ${srcPath} to ${desPath}`)
+          }).catch(e => console.log(`${e}`))
+      }
     }
-
-    const starcw20Namejsonpath = getJsonPath('starcw20Names', configPath)
-    const starcw20names = await loadJson(arrayTemplate, starcw20Namejsonpath)
-    console.log(`load star-names from ${ringcw20Namejsonpath}`)
-    //
-    let starnames: string[] = []
-    Object.entries(starcw20names[networkname]).forEach(data => {
-      const element = String(data[1]);
-      starnames.push(element)
-
-    })
-
-    for (let index = 0; index < starnames.length; index++) {
-      const element = starnames[index];
-      const desPath = srcPath.replace(`logos/LOGO.${type}`,`icons/${element}.${type}`)
-      await copyFile(srcPath,desPath)
-      .then(() => {
-        console.log(`Copy ${srcPath} to ${desPath}`)
-      }).catch(e => console.log(`${e}`))
-    }
-
-
   });
 }
 
+function isFilesExists(filePath: string):boolean  {
+  // Check if the file exists
+  try {
+    fs.accessSync(filePath, fs.constants.F_OK);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+async function createDirectoryIfNotExists(directoryPath: string): Promise<void> {
+  console.log(directoryPath)
+  if (!fs.existsSync(directoryPath)) {
+    fs.mkdirSync(directoryPath);
+    console.log(`Directory created: ${directoryPath}`);
+  } else {
+    console.log(`Directory already exists: ${directoryPath}`);
+  }
+};
 
 function copyFile(sourcePath: string, destinationPath: string): Promise<void> {
   return new Promise<void>((resolve, reject) => {
